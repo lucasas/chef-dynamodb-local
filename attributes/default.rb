5b313c330b7e3381007ba2552d3057ec90219f10
Lucas Souza <lucasas@gmail.com>
2016-03-15 09:37:37 -0300

    Allowing extra options like sharedDb, inMemory, etc

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,6 @@ default["dynamodb-local"] = {
   "directory" => "/usr/local/lib/dynamodb-local",
   "log_dir" => "/var/log/dynamodb-local",
   "download_url" => "http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest",
-  "port" => 8000
+  "port" => 8000,
+  "extra_options" => ""
 }
